3e151a39940700036c7163a73c4b349e1249c346
karishma24-max <chauhankarishma824@gmail.com>
2022-11-02 03:44:15 +0530

    added

--- script.js
+++ script.js
@@ -15,4 +15,27 @@ $('.navbar').addClass("sticky")
         $('.navbar .menu ').toggleClass("active")
         $('.menu-btn i ').toggleClass("active")
     });
+
+    // <owl carousel script>
+    $('.carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplay:true,
+        autoplayTimeOut:2000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                 items:3,
+                nav:false
+            }
+        }
+    })
 })
