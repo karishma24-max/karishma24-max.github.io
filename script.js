e00941749e78403801724c1e246b1e6d2f30d2bb
karishma24-max <chauhankarishma824@gmail.com>
2022-12-07 02:56:28 +0530

    added

--- script.js
+++ script.js
@@ -22,6 +22,16 @@ $('.scroll-up-btn').click(function(){
         $('.navbar .menu ').toggleClass("active")
         $('.menu-btn i ').toggleClass("active")
     });
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    // $('.menu-btn').click(function(){
+    //     $('.navbar .menu').toggleClass("active");
+    //     $('.menu-btn i').toggleClass("active");
+    // });
 {/* <typing animation script> */}
 
 var typed = new Typed(".typing", {
